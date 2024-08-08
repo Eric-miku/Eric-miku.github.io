742f05c295ba8df35880e86266cd1187cf381a54
Eric-miku <2446190505@qq.com>
2024-08-08 23:22:39 +0800

    Add files via upload

---
+++ web_page_1/scripts/main.js
@@ -0,0 +1,5 @@
+let myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+document.querySelector("html").addEventListener("click", function () {
+    alert("别戳我，QWQ。");
+  });
